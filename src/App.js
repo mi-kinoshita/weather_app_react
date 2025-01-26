4dd16d181897d21c1427b83938fed70665bac9da
mi-kinoshita <mksu777@gmail.com>
2025-01-26 16:14:44 +0900

    Set up this project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
   const [weatherData, setWeatherData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [city, setCity] = useState("tokyo");
 
-  const city = "tokyo";
+  const inputCity = (event) => {
+    setCity(event.target.value);
+  };
+
+  const cityName = "tokyo";
   const apiKey = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
 
   useEffect(() => {
     const fetchWatherData = async () => {
       try {
         const res = await axios.get(
-          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+          `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`
         );
 
         setWeatherData(res.data);
@@ -36,6 +41,15 @@ function App() {
       <h1>Weather in {weatherData.name}</h1>
       <p>Temperature: {weatherData.main.temp}°C</p>
       <p>Condition: {weatherData.weather[0].description}</p>
+      <br />
+      <p>都市を選んでください。</p>
+      <input
+        type="text"
+        value={city}
+        onChange={inputCity}
+        placeholder="都市を入力してください"
+      />
+      <button onClick={setWeatherData}>API Call</button>
     </div>
   );
 }
